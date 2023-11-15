6384f5e4447afb2529c6eee837aa833a75a2a7b2
unknown <dmstpals104@ajou.ac.kr>
2023-11-15 21:45:56 +0900

    fix:Fix the sound

---
+++ Assets/Scripts/Sound/BGMController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class BGMController : MonoBehaviour
+{
+    static public BGMController instance;
+    public GameObject BGM;
+    public AudioSource audio;
+
+
+    void Start()
+    {
+        audio = BGM.GetComponent<AudioSource>();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(BGM);    //keep playing audio when the scene is changed
+        }
+        else Destroy(BGM); //keep playing audio when the scene is changed
+    }
+
+    void Update()
+    {
+
+    }
+}
